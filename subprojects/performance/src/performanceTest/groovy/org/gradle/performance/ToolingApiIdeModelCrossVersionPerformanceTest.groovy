6ed5ed94043a5f310a28cbee1d5b00a02ff365ae
Lari Hotari <lari.hotari@gradle.com>
2016-11-08 12:22:03 +0200

    Rebase TAPI perf test

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/ToolingApiIdeModelCrossVersionPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/ToolingApiIdeModelCrossVersionPerformanceTest.groovy
@@ -37,7 +37,7 @@ class ToolingApiIdeModelCrossVersionPerformanceTest extends AbstractToolingApiCr
             invocationCount = 30
             sleepAfterTestRoundMillis = 25
             // rebaselined because of https://github.com/gradle/performance/issues/99
-            targetVersions = ['3.2-rc-1']
+            targetVersions = ['3.3-20161108000031+0000']
             action {
                 def model = model(tapiClass(EclipseProject))
                     .setJvmArguments(createDefaultJvmOptions()).get()
@@ -184,10 +184,10 @@ class ToolingApiIdeModelCrossVersionPerformanceTest extends AbstractToolingApiCr
         where:
         // rebaselined because of https://github.com/gra
         template          | targetGradleVersions
-        "smallOldJava"    | ['3.2-rc-1']
-        "mediumOldJava"   | ['3.2-rc-1']
-        "bigOldJava"      | ['3.2-rc-1']
-        "lotDependencies" | ['3.2-rc-1']
+        "smallOldJava"    | ['3.3-20161108000031+0000']
+        "mediumOldJava"   | ['3.3-20161108000031+0000']
+        "bigOldJava"      | ['3.3-20161108000031+0000']
+        "lotDependencies" | ['3.3-20161108000031+0000']
     }
 
     private static void forEachEclipseProject(def elm, @DelegatesTo(value=EclipseProject) Closure<?> action) {
